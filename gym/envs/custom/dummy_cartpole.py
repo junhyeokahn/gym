08290272ff16d77783e2082c0607437462171e29
junhyeokahn <junhyeokahn91@gmail.com>
2019-02-18 10:06:48 -0600

    fix dummy cartpole env

--- gym/envs/custom/dummy_cartpole.py
+++ gym/envs/custom/dummy_cartpole.py
@@ -18,13 +18,13 @@ class DummyCartPoleEnv(gym.Env):
         cfg_path = os.getcwd() + '/Config/CartPole/TEST/RL_TEST.yaml'
         with open(cfg_path) as f:
             config = yaml.safe_load(f)
-            self.action_lower_bound = config['test_configuration']['action_lower_bound']
-            self.action_upper_bound = config['test_configuration']['action_upper_bound']
+            self.action_lower_bound = np.array(config['test_configuration']['action_lower_bound'])
+            self.action_upper_bound = np.array(config['test_configuration']['action_upper_bound'])
             self.action_scale = config['test_configuration']['action_scale']
             self.obs_lower_bound = np.array(config['test_configuration']['obs_lower_bound'])
             self.obs_upper_bound = np.array(config['test_configuration']['obs_upper_bound'])
 
-        self.action_space = spaces.Box(low=self.action_lower_bound, high=self.action_upper_bound, shape=(1,), dtype=np.float32)
+        self.action_space = spaces.Box(low=self.action_lower_bound, high=self.action_upper_bound, dtype=np.float32)
         self.observation_space = spaces.Box(low=self.obs_lower_bound, high=self.obs_upper_bound, dtype=np.float32)
     def step(self, action):
         pass
